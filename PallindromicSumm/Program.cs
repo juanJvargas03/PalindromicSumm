e1ba50bfe1608ba8e9302cee5ed23acaba877daa
Juan Jose Vargas Vargas <juan.03.jvargas@gmail.com>
2023-05-04 19:03:05 -0500

    [Functionality] Add functionality to sum palindromic numbers in decimal and binary base

---
+++ PallindromicSumm/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sum = 0;
+        for (int i = 1; i <= 1000000; i++)
+        {
+            if (IsPalindromic(i, 10) && IsPalindromic(i, 2))
+            {
+                Console.WriteLine(i + " is a palindromic number");
+                Console.WriteLine("and in base 2 is also a palindrome " + Convert.ToString(i, 2));
+                sum += i;
+            }
+        }
+        Console.WriteLine("The sum of all palindromic numbers in base 10 and binary up to one million is: " + sum);
+        Console.ReadLine();
+    }
+
+    static bool IsPalindromic(int number, int baseNumber)
+    {
+        string numberString = Convert.ToString(number, baseNumber);
+        int length = numberString.Length;
+        for (int i = 0; i < length / 2; i++)
+        {
+            if (numberString[i] != numberString[length - i - 1])
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# PalindromicSumm
+C# Console App Project
+This is a C# console app project that sums all the palindromic numbers in base 10 and binary from 0 to 1,000,000 (excluding those with leading zeros) and prints the result to the console.
+
+Prerequisites
+.NET 6 SDK
+How to Run
+Clone or download this repository to your local machine.
+Open the solution file PalindromicNumbers.sln in Visual Studio 2022.
+Build the solution to restore NuGet packages and compile the code.
+Press F5 or select Debug > Start Debugging from the menu to run the application.
+The application will print the sum of all the palindromic numbers in base 10 and binary from 0 to 1,000,000 to the console.
+License
+This project is licensed under the MIT License. Feel free to use, modify, and distribute the code for any purpose.
